b7e1486f302219b77a2fb0f6cc26b8bc703ef639
KBryt <effahbright.eb@gmail.com>
2022-12-09 17:23:29 -0800

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main function to generatea random number
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* My code is here*/
+	
+	if ( n > 0 )
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is Zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return(0);
+
+
+}
+		
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+A while loop in C programming repeatedly executes a target statement as long as a given condition is true.
