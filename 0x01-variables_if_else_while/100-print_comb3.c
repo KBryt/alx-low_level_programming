e995f17f546701e6dc7a4353734cc203937c0d9c
KBryt <effahbright.eb@gmail.com>
2022-12-09 22:36:02 -0800

    Task adv

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, e;
+	
+	a = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		a = 48;
+		while (a < 58)
+		{
+			if (e != a && e < a)
+			{
+				putchar(e);
+				putchar(a);
+				if (a == 57 && e ==56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			a++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
