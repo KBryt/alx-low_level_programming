241e258606f75aa9a4841bf73c77d20f2d5136d1
KBryt <effahbright.eb@gmail.com>
2022-12-11 10:15:59 -0800

    Task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,18 +15,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%i is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%i is negative\n", n);
 	}
 	return (0);
 }	
