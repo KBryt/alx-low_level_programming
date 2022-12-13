ad75cba2e04739c771c6c4b06057d56caa6254cb
KBryt <effahbright.eb@gmail.com>
2022-12-13 03:33:01 -0800

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * _isalpha - Return 1 if c is a letter. lowercase or uppercase
  * @c: unsigned int value to be compared with ASCII value
  * Return: 1 if ints alphabet otherwise 0
