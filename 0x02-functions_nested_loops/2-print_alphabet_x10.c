7368afbd67ea5862f211534f2ed8970ce7b65ce0
KBryt <effahbright.eb@gmail.com>
2022-12-13 02:39:24 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; i++)
+		{
+			_putchar(c);
+		}
+	}
+}
