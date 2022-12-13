4bc9803d705f4287753eb59be442ed214f89b375
KBryt <effahbright.eb@gmail.com>
2022-12-13 02:48:31 -0800

    TASK 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,9 +12,11 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (c = 'a'; c <= 'z'; i++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
+		
+		_putchar('\n');
 	}
 }
