7f96d7b816e9f19e3de595004005ac7d733d9c3d
KBryt <effahbright.eb@gmail.com>
2022-12-13 03:50:25 -0800

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - return 0 letter not loercase, 1 letter lowercase
+ *
+ * @n : number to check
+ * Return: 0 or 1
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
