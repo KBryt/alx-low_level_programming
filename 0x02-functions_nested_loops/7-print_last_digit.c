83d1b7da0f272877bc4a8da48f1701cceed20072
KBryt <effahbright.eb@gmail.com>
2022-12-13 16:08:38 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,12 @@
 
 /**
  * print_the_last_digit - print the last digit of the number
- * @n: The last digit in the question
+ * Description: @n The last digit in the question
  * Return: returns the value of the last digit
  */
 
 int print_last_digit(int n)
+
 {
 	int last_digit = n % 10;
 
