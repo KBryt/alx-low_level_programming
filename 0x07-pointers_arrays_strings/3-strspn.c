3d7e0181d2c017313fca0188470cb1bbd28cd762
KBryt <effahbright.eb@gmail.com>
2023-01-02 17:12:32 -0800

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - return length of string that matches values consistently
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j;
+	int matches = 0;
+
+	while (s[i] != '\0') /*iterate through target*/
+	{
+		for (j = 0; accept[j] != '\0'; j++) /*iterate through traget*/
+		{
+			if (s[i] == accept[j]) /*record & break & break at first match*/
+			{
+				matches++;
+				break;
+			}
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+
+			return (matches);
+		}
+		i++;
+	}
+	return (matches);
+}
