434f7688d48ce8ec09ae3446b3f76324b76c9c84
KBryt <effahbright.eb@gmail.com>
2023-01-06 17:01:40 -0800

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: nummber of arguments
+ * @argv: array of orguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv)
+{
+	(void) argv; /*Ignore argv*/
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
