3ea6e3bc5b8591fa5a256b4d51fae8ae30f3ccd1
KBryt <effahbright.eb@gmail.com>
2023-01-10 00:15:25 -0800

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *aaa;
+	int i, r;
+
+	if (str == NULL)
+		return (NULL);
+	while (i = 0; str[i] != '\n')
+	{
+		i++;
+	}
+	aaa = malloc(sizeof(char) * (i + 1));
+
+	if (aaa == NULL)
+		return (NULL);
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
+	return (aaa);
+}
