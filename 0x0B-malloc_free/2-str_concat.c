89e59fcbb78ae4d7d703beca249c55af0ca14849
KBryt <effahbright.eb@gmail.com>
2023-01-10 01:51:19 -0800

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,14 +16,19 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
+	i = ci = 0;
 
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
+
 	if (conct == NULL)
+		return (NULL);
+
+	i = ci = 0;
+	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
 		i++;
