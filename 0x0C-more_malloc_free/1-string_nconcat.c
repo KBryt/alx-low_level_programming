843c47e09a1e15499d3b54afe87d8f1be115c9a7
KBryt <effahbright.eb@gmail.com>
2023-01-12 09:53:55 -0800

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,6 +34,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (p == NULL)
 		return (0);
 
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
 	for (; i < (size1 + n); i++)
 	{
 		p[i] = s2[i - size1];
