517b3ed06d076bb1b971191e41cafa64cd372bae
KBryt <effahbright.eb@gmail.com>
2023-01-26 18:33:24 -0800

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	int nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
-
+		}
 		nodes++;
 		h = h->next;
 	}
