ae7bb861938f91c1d62cb2c8b18b19574d49590e
KBryt <effahbright.eb@gmail.com>
2023-01-26 18:57:47 -0800

    Task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
- * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
- * is executed before main()
- * Return: no return
- */
-void __attribute__ ((constructor)) bmain()
+* beforeMain - Runs before the main fucntion
+*/
+
+void __attribute__((constructor)) beforeMain()
 {
-	printf("You 're beat! and yet, you must allow,\n");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n%s",
+			"I bore my house upon my back!\n");
 }
