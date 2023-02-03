7d20581119a053a86dcc9627ef5af196549e27de
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-02-02 19:42:37 -0500

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
   * binary_to_uint - converts a binary number to an unsigned int
-  * @b: binary number
+  * @b: binary number.
   * Return: the converted number or 0 if it fails
   */
 unsigned int binary_to_uint(const char *b)
