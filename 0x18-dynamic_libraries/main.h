3ddb25588d852f4004c880eb180ea3efcefd1ed2
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-03-20 22:53:27 -0400

    Update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
-#include <stdio.h>
-#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,10 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int mod(int a, int b);
-extern div_t div(int __numer, int __denom);
+#endif
 
-#endif /* MAIN_H */
