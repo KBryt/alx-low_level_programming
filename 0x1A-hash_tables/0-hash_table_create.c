45ce1bacabac3ce248532a90ebd016e388e78a35
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-04-07 22:48:01 -0400

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Fxn that creates a hash table.
+ * hash_table_create - Creates a hash table.
  * @size: The size of the array.
  *
- * Return: If an error occurs - NULL otherwise pointer to the new hash table.
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -20,8 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-	{
 		ht->array[i] = NULL;
-	}
+
 	return (ht);
 }
