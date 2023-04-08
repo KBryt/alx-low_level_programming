50bd3da09ef4c3bfe149b4e60dd50b078ff41d7c
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-04-07 22:54:04 -0400

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,19 +8,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+	hash_table_t *newtable;
+	hash_node_t **node;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	newtable = calloc(1, sizeof(hash_table_t));
+	if (newtable == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+	newtable->size = size;
+	newtable->array = calloc(size, sizeof(node));
 
-	return (ht);
+	if (newtable->array == NULL)
+		return (NULL);
+	return (newtable);
 }
