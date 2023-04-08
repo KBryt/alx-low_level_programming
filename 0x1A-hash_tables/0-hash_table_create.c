15c05ee24b4a08c2131370142f421d6cc81019ef
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-04-07 21:01:26 -0400

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,7 +5,7 @@
  * @size: The size of the array.
  *
  * Return: If an error occurs - NULL,
-           Otherwise - a pointer to the new hash table.
+ *	   Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
